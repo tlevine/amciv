2e408da87fa5993a33ba8f75f1ca259afe8bf9db
Thomas Levine <occurrence@thomaslevine.com>
2013-11-23 18:49:58 +0000

    import

--- data.py
+++ data.py
@@ -1,21 +1,63 @@
+#!/usr/bin/env python
 '''
-Request URL:https://amciv.lumenogic.com/amciv/jsx.json
+Request URL:
 Request Method:POST
 Status Code:200 OK
 
-jsx:[[["GQ","getById",{"id": 1}],"format",{"fmt": {"account": {"cash": true}}}],[["GQ","getById",{"id": 205}],"format",{"fmt": {"cond": {"account": {"cash": true},"otcm": {"account": {"cash": true},"hldg": {"cash": true,"order": {"user": {"idOnly": true}}},"orderbook": {"user": {"idOnly": true}},"trade": {"summary": true},"tradesSince": 1}},"otcm": {"account": {"cash": true},"hldg": {"cash": true,"order": {"user": {"idOnly": true}}},"orderbook": {"user": {"idOnly": true}},"trade": {"summary": true},"tradesSince": 1}}}],[["forum","get",{"topic": "Q.205"}],"format",{"fmt": {"post": {"login": true},"since": 1}}]]
 '''
+
+from hashlib import sha512
+import json
 
 from requests import session
 
-s = session()
-s.headers['Accept'] = '*/*'
-s.headers['Accept-Encoding'] = 'gzip,deflate,sdch'
-s.headers['Accept-Language'] = 'en-US,en;q=0.8,fr;q=0.6,sv;q=0.4,zh;q=0.2,zh-CN;q=0.2,zh-TW;q=0.2'
-s.headers['Connection'] = 'keep-alive'
-s.headers['Content-Length'] = '1191'
-s.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
-s.headers['Host'] = 'amciv.lumenogic.com'
-s.headers['Origin'] = 'https://amciv.lumenogic.com'
-s.headers['Referer'] = 'https://amciv.lumenogic.com/amciv/app.html'
-s.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36'
+class Jsx:
+    url = 'https://amciv.lumenogic.com/amciv/jsx.json'
+    def __init__(self, login = 'guest', password = 'american'):
+        s = session()
+        s.headers['Accept'] = '*/*'
+        s.headers['Accept-Encoding'] = 'gzip,deflate,sdch'
+        s.headers['Accept-Language'] = 'en-US,en;q=0.8,fr;q=0.6,sv;q=0.4,zh;q=0.2,zh-CN;q=0.2,zh-TW;q=0.2'
+        s.headers['Connection'] = 'keep-alive'
+        s.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
+        s.headers['Host'] = 'amciv.lumenogic.com'
+        s.headers['Origin'] = 'https://amciv.lumenogic.com'
+        s.headers['Referer'] = 'https://amciv.lumenogic.com/amciv/app.html'
+        s.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36'
+
+        r = s.get('https://amciv.lumenogic.com/amciv/app.html')
+
+        r = s.post(self.url, data = {
+            'jsx':json.dumps([["registrar","login",{"login": login,"authKey": sha512(password).hexdigest()}]])
+        })
+        s.cookies.set('lumAuth',json.loads(r.text)[0])
+        self.session = s
+    def lookup(self):
+        r = self.session.post(self.url, data = {
+            'jsx':json.dumps([
+                [["GQ","getById",{"id": 1}],"format",{"fmt": {"account": {"cash": True}}}],
+                [
+                    ["GQ","getById",{"id": 205}],
+                    "format",
+                    {"fmt": {
+                        "cond": {
+                            "account": {"cash": True},
+                            "otcm": {
+                                "account": {"cash": True},
+                                "hldg": {"cash": True,"order": {"user": {"idOnly": True}}},
+                                "orderbook": {"user": {"idOnly": True}},
+                                "trade": {"summary": True},"tradesSince": 1}
+                            },
+                        "otcm": {
+                            "account": {"cash": True},
+                            "hldg": {"cash": True,"order": {"user": {"idOnly": True}}},
+                            "orderbook": {"user": {"idOnly": True}},
+                            "trade": {"summary": True},"tradesSince": 1}
+                        }
+                    }
+                ],
+                [["forum","get",{"topic": "Q.205"}],"format",{"fmt": {"post": {"login": True},"since": 1}}]
+              ])
+            }
+        )
+
